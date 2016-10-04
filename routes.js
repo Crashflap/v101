32c0fd3385490a920905262247fe02d1d77c1262
Crashflap <support@crashflap.com>
2016-10-05 04:04:14 +1100

    Add files via upload

---
+++ routes.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {Route} from 'react-router';
+import ConfigurationPage from './containers/ConfigurationPage';
+
+export default (
+    <Route path="/" component={ConfigurationPage}></Route>
+);
